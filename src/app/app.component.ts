7aa2243ee142dabc5f560726491569680cc4dac1
sanjay <skumarverma45@gmail.com>
2020-01-19 21:23:24 +0530

    added working example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
     visiableLightbox: boolean = false;
     currentIndex: number = 0;
+    currentObj = [];
 
     title = 'app';
     imageObject = [{
@@ -25,12 +26,26 @@ export class AppComponent {
         title: 'title of image seven'
     }];
 
-    showLightbox(index) {
+    imageObject1 = [{
+        video: 'https://youtu.be/6pxRHBw-k8M',
+        posterImage: 'https://img.youtube.com/vi/6pxRHBw-k8M/hqdefault.jpg'
+    }, {
+        video: 'https://youtu.be/tYa6OLQHrEc',
+        posterImage: 'https://img.youtube.com/vi/tYa6OLQHrEc/hqdefault.jpg'
+    }, {
+        image: 'assets/img/slider/4.jpg',
+        thumbImage: 'assets/img/slider/4_min.jpeg'
+    }];
+
+    showLightbox(object, index) {
         this.currentIndex = index;
+        this.currentObj = object;
+        console.log('=>', this.currentObj, this.currentIndex)
         this.visiableLightbox = true;
     }
 
     closeHandler() {
         this.visiableLightbox = false;
+        this.currentIndex = -1;
     }
 }
